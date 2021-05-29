97595caed93cc10e26dc4edc6632739b6d210b8e
Mohammed Rafi M. V <mohammedr@data-matica.com>
2021-05-29 10:33:19 +0530

    Skelton for Api and Ui

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: "YOUR_GOOGLE_MAPS_API_KEY",
+    }),
+  ],
+  declarations: [AppComponent, AdminLayoutComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ client/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { IconsComponent } from '../../icons/icons.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+import { TableListComponent } from '../../table-list/table-list.component';
+import { UpgradeComponent } from '../../upgrade/upgrade.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { AdminLayoutRoutes } from './admin-layout.routing';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule,
+  ],
+  declarations: [
+    DashboardComponent,
+    UserProfileComponent,
+    TableListComponent,
+
+    IconsComponent,
+
+    NotificationsComponent,
+    UpgradeComponent,
+  ],
+})
+export class AdminLayoutModule {}
